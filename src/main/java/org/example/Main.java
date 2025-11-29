9d20c89e1d50e1c3ce5e2a5067408bc0082ef9e1
wcx <1165990138@qq.com>
2025-11-29 15:36:02 +0800

    wcx1-1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) {
         //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
         // 查看 IntelliJ IDEA 建议如何修正。
-
+        System.out.println("wcx1");
     }
 }
